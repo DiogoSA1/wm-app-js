2cf595f60843e806f69ad2522cbab6e688f24801
Diogo <albuquerquediogo@hotmail.com>
2024-01-06 17:42:14 -0300

    Implementação dos métodos de validação e api de usuarios

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,4 +1,9 @@
 module.exports = app => {
     app.route('/users')
         .post(app.api.user.save)
+        .get(app.api.user.get)
+
+        app.route('/users/:id')
+        .get(app.api.user.getById)
+        .put(app.api.user.save)
 }
--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,18 +1,16 @@
 module.exports = {
-  development: {
 
-    client: 'postgresql',
-    connection: {
-      database: 'backendjs',
-      user: 'postgres',
-      password: '123456'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+  client: 'postgresql',
+  connection: {
+    database: 'backendjs',
+    user: 'postgres',
+    password: '123456'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
   }
 };
