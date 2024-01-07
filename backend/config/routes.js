342128f2ef207ae776c4d1d2a14afff3d3eef6c7
Diogo <albuquerquediogo@hotmail.com>
2024-01-06 23:27:54 -0300

    Implementação api de categorias

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -3,7 +3,19 @@ module.exports = app => {
         .post(app.api.user.save)
         .get(app.api.user.get)
 
-        app.route('/users/:id')
+    app.route('/users/:id')
         .get(app.api.user.getById)
         .put(app.api.user.save)
+
+    app.route('/categories')
+        .get(app.api.category.get)
+        .post(app.api.category.save)
+
+    app.route('/categories/tree')
+        .get(app.api.category.getTree)
+        
+    app.route('/categories/:id')
+        .get(app.api.category.getById)
+        .put(app.api.category.save)
+        .delete(app.api.category.remove)
 }
