58df66d5db72210e1a5b770ec544840fb6bbc36d
Diogo <albuquerquediogo@hotmail.com>
2024-01-23 01:00:26 -0300

    inicio implementacao do docker

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/knowledge_stats', {useNewUrlParser: true})
+mongoose.connect('mongodb://localhost/knowledge_stats', {useNewUrlParser: true, useUnifiedTopology: true})
     .catch(e => {
         const msg = 'ERRO! Não foi possivel conectar com MongoDB!'
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
